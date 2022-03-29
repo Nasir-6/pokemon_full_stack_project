8f43376312ca109532a3b23fc703e0001508d30a
Shirhan <shirhan25@hotmail.com>
2022-03-29 13:13:52 +0100

    pseudo code Services class

---
+++ pokemon-back-end/src/main/java/com/pokepals/pokemonbackend/pokemon/PokemonService.java
@@ -0,0 +1,35 @@
+package com.pokepals.pokemonbackend.pokemon;
+
+public class PokemonService {
+
+
+  //instance of the Pokemon DAO
+
+
+
+  //1. Add Pokemon
+    //if statement to check if the id is already in the database
+    // but if the exists - you already have this pokemon, don't catch!
+
+  //2. Get all Pokemons by Trainer Id
+    //Using a List
+    //length/size of list == 0 then no pokemons
+    //illegal state exception
+
+  //3. Delete Pokemons by Id
+//if statement - check if (PokemonDAO.selectPokemonById == null))
+    //exception - pokemon is not there
+    // else return the pokemon to be deleted (by Id)
+
+
+
+  //4. Update Pokemon (Extension)
+    //update HP
+    //update Level
+//if statement - to check if  the pokemon is there (PokemonDAO.selectPokemonById != null)
+    //else exception - pokemon is not there
+
+
+
+
+}
