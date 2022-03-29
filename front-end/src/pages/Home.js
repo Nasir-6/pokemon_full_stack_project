20095d03ba9f52a417e05420d9f49426f3e24fab
Nasir <nasir-islam@hotmail.com>
2022-03-29 18:10:03 +0100

    make getSpriteUrl

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,6 +1,27 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
+
+    const [spriteUrl, setSpriteUrl] = useState("")
+
+    // fetch a pokemon and console.log the url
+    const grabPokemonSprite = async () => {
+        const newSpriteUrl = await fetch("https://pokeapi.co/api/v2/pokemon/1/")
+        .then(response => response.json())
+        .then(onePokemon => onePokemon.sprites.front_default)
+
+        setSpriteUrl(newSpriteUrl);
+
+    }
+
+    useEffect(()=>{
+        grabPokemonSprite();
+    },[])
+
+    
+
+
   return (
     <h2>Home</h2>
   )
