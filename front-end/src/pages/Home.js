21b465fa377df7fbba7adf91b2ca203ea53408c4
Rachel Kaur <rachel.km@outlook.com>
2022-03-29 19:29:17 +0100

    test user sprite and add favicon

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import userSprite from '../images/ash_sprite.png'
 
 export default function Home() {
 
   return (
     <>
-      <h2>Home</h2> 
+      <h2>Home</h2>
+      <div className='userSprite'>
+        <img src = {userSprite} alt = "User sprite"/>
+      </div> 
     </>
   );
 }
