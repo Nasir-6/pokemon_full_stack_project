4477a8542666a83b810c70aac0685e2c827c5161
Nasir <nasir-islam@hotmail.com>
2022-03-31 00:46:23 +0100

    map, move buttons and boundary conditions added

--- front-end/src/pages/Map.js
+++ front-end/src/pages/Map.js
@@ -1,14 +1,88 @@
-import React from 'react'
-import WildPokemon from '../containers/WildPokemon'
+import React from "react";
+import WildPokemon from "../containers/WildPokemon";
+import userSprite from "../images/ash_sprite.png";
+import { useEffect, useState } from "react";
 
 export default function Map() {
+  const [userPosition, setUserPostion] = useState(6);
 
-  
+  let gridIndexes = [];
+  const GRIDLENGTH = 10;
+  for (let i = 1; i <= GRIDLENGTH ** 2; i++) {
+    gridIndexes.push(i);
+  }
+
+  const grids = gridIndexes.map((index) => {
+    if (index == userPosition) {
+      return <div className={`oneGrid grid-${index} user-grid`}></div>;
+    } else {
+      return <div className={`oneGrid grid-${index}`}></div>;
+    }
+  });
+
+  const moveRight = () => {
+    const isOnRightEdge = (userPosition%GRIDLENGTH == 0)
+    if(!isOnRightEdge){
+      setUserPostion(userPosition+1)
+    } else{
+      console.log('User is on the right edge');
+    }
+  }
+
+  const moveLeft = () => {
+
+    const isOnLeftEdge = (userPosition%GRIDLENGTH == 1)
+    if(!isOnLeftEdge){
+      setUserPostion(userPosition-1)
+    } else{
+      console.log('User is on the left edge');
+    }
+  }
+
+  const moveUp = () => {
+    const isOnTopEdge = (userPosition>0 && userPosition<=GRIDLENGTH)
+    if(!isOnTopEdge){
+      setUserPostion(userPosition-GRIDLENGTH)
+    } else{
+      console.log('User is on the top edge');
+    }
+    
+  }
+
+  const moveDown = () => {
+    const isOnBottomEdge = (userPosition>(GRIDLENGTH*(GRIDLENGTH-1)) && userPosition<=GRIDLENGTH**22)
+    if(!isOnBottomEdge){
+      setUserPostion(userPosition+GRIDLENGTH)
+    } else{
+      console.log('User is on the bottom edge');
+    }
+    
+  }
+
+
+
+
+
+
+
+
 
 
   return (
-    <>
-    <WildPokemon/>
-    </>
-  )
+    <div className="map-page">
+      <div className="grid-container">{grids}</div>
+
+      <div className="moves-container">
+        <button className="btn up-btn" onClick={moveUp}>Up</button>
+        <button className="btn right-btn" onClick={moveRight}>Right</button>
+        <button className="btn down-btn" onClick={moveDown}>Down</button>
+        <button className="btn left-btn" onClick={moveLeft}>Left</button>
+        <img src={userSprite} alt="User sprite" className="user-sprite" />
+      </div>
+
+      <div className="wild-pokemon-container">
+        <WildPokemon />
+      </div>
+    </div>
+  );
 }
