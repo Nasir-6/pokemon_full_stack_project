43ec82a44604f9bd721a3b0d5de193d3cd74a854
Shirhan <shirhan25@hotmail.com>
2022-03-30 20:47:05 +0100

    nasir changes part 2

--- front-end/src/containers/Navbar.js
+++ front-end/src/containers/Navbar.js
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className= "navbar">Navbar</div>
+    <div className= "navbar">
+    <div className="navbar-left">
+      logo
+      </div>
+    <div className="navbar-right">
+      homepage button (className="button" )
+
+    {/* making login and signup button component */}
+   
+     <loginSignUp 
+     />
+     </div>
+    
+    </div>
+
+    
   )
 }
