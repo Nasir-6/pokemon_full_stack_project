6b3929b8bc87910a30b678bef26e5687882333e3
Hajr Delli <hajr_delli1@hotmail.co.uk>
2022-03-31 18:05:56 +0100

    login button css

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -4,10 +4,15 @@ export default function Login() {
   return (
     <div className='login-page'>
       <h1>Login</h1>
-      <form action="" className='login-form'>
 
+      <form  action="Name" className='login-form'>
+      <input type="text"  className="inputs" placeholder="Enter email address"></input>
+
+      <input type="text"  className="inputs" placeholder="Enter password"></input>
 
       </form>
+
+      <button>Login</button>
 
       <img src="" alt="" />
 
