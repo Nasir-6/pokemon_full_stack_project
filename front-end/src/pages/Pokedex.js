ff9db77ee8bb19fab2164848bb5447377be0e56d
Hajr Delli <hajr_delli1@hotmail.co.uk>
2022-04-01 05:01:23 +0100

    pokedex divs and title added

--- front-end/src/pages/Pokedex.js
+++ front-end/src/pages/Pokedex.js
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function Pokedex() {
   return (
+    <>
+    <h2>Manage your pokémon team</h2>
     <div>Pokedex</div>
+
+    <div className="all-cards"></div>
+    <div className="card-container"></div>
+
+
+    
+
+
+    </>
   )
 }
