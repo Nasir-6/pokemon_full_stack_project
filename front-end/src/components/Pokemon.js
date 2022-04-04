9dcc3395c69117c86822d25a93c18b02ef29496d
suraya <suraya.hasan@hotmail.com>
2022-04-04 12:54:41 +0100

    delete btn

--- front-end/src/components/Pokemon.js
+++ front-end/src/components/Pokemon.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 // Passing in the single pokemon as a prop
 export const Pokemon = ({pokemon}) => {
   return (
+    <>
+     <div className="pokemonCard-Container"></div>
+    
+
+   
+    
     <div className="pokemonCard">
+
+      
+      
+
       <h3 className='card-title'>{pokemon.name}</h3>
       <div className='card-img-container'>
         <img src={pokemon.sprite_link} alt= {`Image of ${pokemon.name}`} className="pokemon-sprite" />
@@ -13,6 +25,11 @@ export const Pokemon = ({pokemon}) => {
       <br/>
       <p>{`Pokemon HP: ${pokemon.hp}`}</p>
       <p>{`Pokemon Level: ${pokemon.level}`}</p>
+
+      <button className="delete-btn"> 
+      <FontAwesomeIcon icon={faTrash}/> Delete </button>
+
     </div>
+    </>
   )
 }
