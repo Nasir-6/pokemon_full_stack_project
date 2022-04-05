bd69fde9833a18a87d7cd4f39356e1e9cb8d34f3
Shirhan <shirhan25@hotmail.com>
2022-04-05 13:01:35 +0100

    map and pokedex button

--- front-end/src/containers/Navbar.js
+++ front-end/src/containers/Navbar.js
@@ -4,11 +4,13 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'
 
 
+
 export default function Navbar({currentUser, updateCurrentUser}) {
 
 
   // NEED THIS TO USE NAVIGATE COMMAND
   const navigate = useNavigate();
+  
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -20,14 +22,27 @@ export default function Navbar({currentUser, updateCurrentUser}) {
     }
   }, [currentUser])
 
+ 
 
   const handleLogoutButtonClick = () => {
     console.log('Logging OUt');
     updateCurrentUser(null);
     navigate("/");
+  
+  }
+
+  const handleMapButtonClick = () => {
+    console.log("Map page");
+    navigate("/map");
+  }
+
+  const handlePokedexButtonClick = () => {
+    console.log("Pokedex page");
+     navigate("/pokedex");
   }
 
 
+ 
   return (
     <div className="navbar">
       <div className="navbar-left">
@@ -37,6 +52,8 @@ export default function Navbar({currentUser, updateCurrentUser}) {
       </div>
       <div className="navbar-right">
         <Link to="/" className="btn">Home</Link>
+        { isLoggedIn ? <><button className='btn' onClick={handleMapButtonClick}>Map</button>  <Link to="/map" className="btn">Map</Link> </>: <></> }
+        { isLoggedIn ? <> <button className='btn' onClick={handlePokedexButtonClick}>Pokedex</button> < Link to="/pokedex"  className="btn">Pokedex</Link> </>  : <></> }
         { isLoggedIn ? <button className='btn' onClick={handleLogoutButtonClick}>Logout</button> : <Link to="/login" className="btn">Login</Link>}
         { isLoggedIn ? <></> : <Link to="/signup" className='btn'>Sign up</Link>}
       </div>
