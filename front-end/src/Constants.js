0eafbb6d6fa6b5b29fc4f910641d638aee9c353d
Nasir <nasir-islam@hotmail.com>
2022-05-19 15:50:41 +0100

    update SERVER_URL and fix bug

--- front-end/src/Constants.js
+++ front-end/src/Constants.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = process.env.NODE_ENV == 'production'? "http://localhost:5000" : "http://localhost:8080"
+export const SERVER_URL = process.env.NODE_ENV == 'production'? "http://pokepals-backend.eu-west-2.elasticbeanstalk.com" : "http://localhost:8080"
 // Port 5000 is the aws elastic beanstalk port which we will deploy whole app onto!!
